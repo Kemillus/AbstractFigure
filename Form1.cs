2987632d97187822c70cb9af3524fba0fb5caab2
Kemillus <b.a.a.ss@mail.ru>
2024-10-02 19:20:30 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AbstractFigure
+{
+    public partial class Figure : Form
+    {
+        public Figure()
+        {
+            this.Paint += new PaintEventHandler(Painter);
+            this.Height = 700;
+            this.Width = 700;
+        }
+
+        private void Painter(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+
+        }
+    }
+}
