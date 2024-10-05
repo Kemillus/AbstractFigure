ea87e38e790afdd4d5e6d3f2d363cb268dc263a6
Kemillus <b.a.a.ss@mail.ru>
2024-10-05 12:39:34 +0300

    v1.3

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ namespace AbstractFigure
 {
     public partial class Figure : Form
     {
+        private Pen _pen;
+        private Brush _brush;
+
         public Figure()
         {
             this.Paint += new PaintEventHandler(Painter);
@@ -18,14 +21,16 @@ namespace AbstractFigure
         {
             Graphics g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
+            _pen = new Pen(Color.Black, 5);
+            _brush = new SolidBrush(Color.White);
             PointF startPoint = new PointF(350, 350);
             float radius = 250;
-            Color color = Color.Green;
-            PaintCicle(g, startPoint, radius, color);
-            PaintRectangles(g, startPoint, radius, color);
+
+            PaintCicle(g, startPoint, radius);
+            PaintRectangles(g, startPoint, radius);
         }
 
-        private void PaintRectangles(Graphics g, PointF startPoint, float radius, Color color)
+        private void PaintRectangles(Graphics g, PointF startPoint, float radius)
         {
             int c = 5;
             int angle = -22;
@@ -34,66 +39,86 @@ namespace AbstractFigure
             {
                 if (i < 2)
                 {
-                    PaintRectangle(g, startPoint, radius, color, angle, false);
+                    PaintRectangle(g, startPoint, radius, angle, false, false);
                     angle += 44;
                 }
 
                 else if (i == 2)
                 {
-                    PaintRectangle(g, startPoint, radius, color, 45, true);
-
-                    PaintRectangle(g, startPoint, radius - (radius / 5), color, 45, false);
+                    PaintRectangle(g, startPoint, radius, 45, true, true);
+                    PaintRectangle(g, startPoint, radius - (radius / 5), 45, false, false);
                 }
 
                 else
-                    PaintRectangle(g, startPoint, radius, color, 0, true);
+                {
+                    PaintRectangle(g, startPoint, radius, 0, true, false);
+                    PaintRectangle(g, startPoint, radius - (radius/5), 0, true, true);
+
+                    float r = radius / 1.8f;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        PaintRectangle(g, startPoint, r, 45, false, false);
+                        r -= radius / 5;
+                    }
+                }
             }
         }
 
-        private void PaintLine(Graphics g, PointF startPoint, float radius, Color color, bool isDiagonal)
+        private void PaintLine(Graphics g, PointF startPoint, float radius, bool isDiagonal, int angle)
         {
             if (!isDiagonal)
             {
-                Pen pen = new Pen(color, 2);
                 PointF p1 = new PointF(startPoint.X - startPoint.X / 2, startPoint.Y);
                 PointF p2 = new PointF(startPoint.X + startPoint.X / 2, startPoint.Y);
-                g.DrawLine(pen, p1, p2);
+                g.DrawLine(_pen, p1, p2);
                 p1 = new PointF(startPoint.X, startPoint.Y - startPoint.Y / 2);
                 p2 = new PointF(startPoint.X, startPoint.Y + startPoint.Y / 2);
-                g.DrawLine(pen, p1, p2);
+                g.DrawLine(_pen, p1, p2);
             }
 
             else
             {
-                
+                RotateMatrix(g, startPoint, angle + 45);
+                PointF p1 = new PointF(startPoint.X - radius, startPoint.Y);
+                PointF p2 = new PointF(startPoint.X + radius, startPoint.Y);
+                g.DrawLine(_pen, p1, p2);
+                p1 = new PointF(startPoint.X, startPoint.Y - radius);
+                p2 = new PointF(startPoint.X, startPoint.Y + radius);
+                g.DrawLine(_pen, p1, p2);
             }
         }
 
-        private void PaintCicle(Graphics g, PointF startPoint, float radius, Color color)
+        private void PaintCicle(Graphics g, PointF startPoint, float radius)
         {
-            g.FillEllipse(new SolidBrush(color), startPoint.X - radius, startPoint.Y - radius, radius * 2, radius * 2);
-            g.DrawEllipse(new Pen(Color.White, 6), startPoint.X - radius, startPoint.Y - radius, radius * 2, radius * 2);
+            g.FillEllipse(_brush, startPoint.X - radius, startPoint.Y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(_pen, startPoint.X - radius, startPoint.Y - radius, radius * 2, radius * 2);
         }
 
-        private void PaintRectangle(Graphics g, PointF centerPoint, float radius, Color color, int angle, bool line)
+        private void PaintRectangle(Graphics g, PointF centerPoint, float radius, int angle, bool line, bool isDiagonal)
         {
-            Matrix matrix = new Matrix();
-            matrix.RotateAt(angle, centerPoint);
-            g.Transform = matrix;
+            RotateMatrix(g, centerPoint, angle);
             float rectangleWidth = radius * 1.4f;
             float rectangleHeight = radius * 1.4f;
             float rectangleX = centerPoint.X - (rectangleWidth / 2);
             float rectangleY = centerPoint.Y - (rectangleHeight / 2);
 
-            g.FillRectangle(new SolidBrush(Color.Violet), rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+            g.FillRectangle(_brush, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+            g.DrawRectangle(_pen, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
 
             if (line)
             {
-                PaintLine(g, centerPoint, radius, color, false);
+                PaintLine(g, centerPoint, radius, isDiagonal, angle);
             }
 
-            g.DrawRectangle(new Pen(Color.Black, 2), rectangleX, rectangleY, rectangleWidth, rectangleHeight);
             g.ResetTransform();
+        }
+
+        private void RotateMatrix(Graphics g, PointF centerPoint, int angle)
+        {
+            g.ResetTransform();
+            Matrix matrix = new Matrix();
+            matrix.RotateAt(angle, centerPoint);
+            g.Transform = matrix;
         }
     }
 }
